4f493416e627207e6a8204518a6467c49c380369
Patrick Mutua <pawatechsys@gmail.com>
2026-02-19 00:03:19 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main //Defines the main package
+
+import "fmt" //Imports formatting package
+
+// Program entry point
+func main() {
+    fmt.Println("Hello, I'm Patrick Mutua") //prints message to the terminal
+	fmt.Println("A junior software engineer") //prints message to the terminal
+	fmt.Println("I'm learning Golang") //prints message to the terminal
+}
